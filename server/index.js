d7579dcbee808f61e88d5258423deef6b7e57c7c
Smethan <ethanhargus@gmail.com>
2021-10-12 13:41:14 -0500

    added rollbar support

--- server/index.js
+++ server/index.js
@@ -1,10 +1,18 @@
 const express = require('express')
 const path = require('path')
+const Rollbar = require('rollbar')
+var rollbar = new Rollbar({
+  accessToken: '45de4a680f9e4823bddad3fccc8f85ce',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
+
 
 const app = express()
 
 app.get('/',function(req,res) {
   res.sendFile(path.join(__dirname, '../tictacjs.html'));
+  rollbar.info('successfully served html')
 });
 
 const port = process.env.PORT || 4000
