1fe22d473a401b2352691501208b4a0c00a6d6a0
smethan <ethanhargus@gmail.com>
2022-01-21 15:20:43 -0600

    This should hopefully work

--- server/index.js
+++ server/index.js
@@ -1,19 +1,21 @@
-const express = require('express')
-const path = require('path')
+const express = require("express");
+const path = require("path");
 
+const app = express();
 
-
-const app = express()
-
-app.get('/',function(req,res) {
-  res.sendFile(path.join(__dirname, '../tictacjs.html'));
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "../tictacjs.html"));
 });
 
-app.use('/js', express.static(path.join(__dirname, '../tictacjs.js')))
-app.use('/css', express.static(path.join(__dirname, '../style.css')))
+app.use("/js", express.static(path.join(__dirname, "../tictacjs.js")));
+app.use("/css", express.static(path.join(__dirname, "../style.css")));
+app.use(
+  "/image",
+  express.static(path.join(__dirname, "../meta-screenshot.png"))
+);
 
-const port = process.env.PORT || 5500
+const port = process.env.PORT || 5500;
 
 app.listen(port, () => {
-  console.log(`Listening on port ${port}`)
-})
+  console.log(`Listening on port ${port}`);
+});
